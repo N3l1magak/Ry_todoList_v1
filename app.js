3f80e2eb4fcf8632df65551053ea10ae88bcb4af
N3l1magak <renhouzhang2021@gmail.com>
2022-05-19 21:53:24 -0400

    add list.ejs to include template, revise app.js to achieve weekday display

--- app.js
+++ app.js
@@ -5,16 +5,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
+  // create var day for output
+  var day = "";
 
-  if( currentDay === 6 || currentDay === 0) {
-    res.write("<h1>It is the weekend</h1>");
-  } else {
-    res.sendFile(__dirname + "/index.html");
+  switch(new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
   }
+
+  // passing var from js into ejs (day -> dayOfWeek)
+  res.render("list", {dayOfWeek: day});
 
 });
 
